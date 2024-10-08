9202431a24a3b735e994511335dc84dda40f7e01
gsilber <silber@udel.edu>
2024-10-08 10:00:20 -0400

    added routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import {SecurityRouter} from "./modules/security/security.router";
+import { InventoryRouter } from "./modules/inventory/inventory.router";
+import { OrderRouter } from "./modules/orders/order.router";
 
 export class ApiRouter {
     private router: express.Router = express.Router();
@@ -7,6 +9,8 @@ export class ApiRouter {
     // Creates the routes for this router and returns a populated router object
     public getRouter(): express.Router {
         this.router.use("/security", new SecurityRouter().getRouter());
+        this.router.use("/items",new InventoryRouter().getRouter());
+        this.router.use("/orders",new OrderRouter().getRouter());
         return this.router;
     }
 }
